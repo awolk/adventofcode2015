cb9668651c04dd596eaa66615bc6fc2bd2f1925a
Aidan Wolk <aidanwolk@comcast.net>
2021-11-06 20:45:16 -0700

    Implement day 1

--- ruby/day1.rb
+++ ruby/day1.rb
@@ -4,4 +4,19 @@
 require_relative './aoc'
 
 input = AOC.get_input(1)
-puts(input)
+
+# Part 1
+lparen = input.count('(')
+rparen = input.count(')')
+end_floor = lparen - rparen
+puts "Part 1: #{end_floor}"
+
+# Part 2
+current_floor = 0
+input.each_char.with_index do |char, index|
+  current_floor += { '(' => 1, ')' => -1 }.fetch(char)
+  if current_floor == -1
+    puts "Part 2: #{index + 1}"
+    break
+  end
+end
